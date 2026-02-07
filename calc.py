233949059982d3c09bbe175677cd4c17abe65561
salman1256@yahoo.com <salman1256@yahoo.com>
2026-02-07 15:25:08 +0800

    after modification

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
+from re import match
+
+
 def add(num1, num2):
     return num1+num2
 def multi(num1, num2):
@@ -11,4 +14,22 @@ print('Calculator Example')
 
 fnum=float(input( 'enter first number: '))
 snum=float(input( 'enter second number: '))
-print ('Result after addition: ',add(fnum,snum))
+
+print('Select Operation: ')
+print('1.Add')
+print('2.Sub')
+print('3.Multi')
+print('4.Div')
+choice=input('Enter choice (1/2/3/4): ')
+match choice:
+    case "1":
+        print('Addition Result: ',add(fnum,snum))
+    case "2":
+        print('Subtraction Result: ',sub(fnum,snum))
+    case "3":
+        print('Multiplication Result: ',multi(fnum,snum))
+    case "4":
+        print('Division Result: ',div(fnum,snum))  
+    case _:
+        print('Invalid Choice')              
+
